105ac07ed920e6994f54d3ea2ffed68c47696f65
bqliu <bqliu@thoughtworks.com>
2017-10-27 17:45:38 +0800

    Add src codes.

---
+++ todolist-frontend/client/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import request from 'superagent';
+import '../style/index.less';
+import ShowItemLists from './ShowItemLists';
+import AddListItem from "./AddListItem";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemLists: []
+    }
+  }
+
+  getIndexOfItem(id){
+    const itemLists = this.state.itemLists;
+    return itemLists.indexOf(itemLists.find(item => item.id === id));
+  }
+
+  componentDidMount() {
+    request.get('http://192.168.33.30:80/todoitems')
+      .end((err, res) => {
+        if (err) {
+          return;
+        }
+        this.setState({itemLists: res.body.items});
+      })
+  }
+
+  addListItem (value){
+    // request.post('http://192.168.30.11/todoitems')
+    //   .send(value)
+    //   .end((err, res)=>{
+    //     if (err) {
+    //       return err;
+    //     }
+    //
+    //     this.state.itemLists.push(res.body.todoItem);
+    //   })
+  }
+
+  updateListItem(id) {
+    // request.put(`http://192.168.30.11/todoitems/${id}`)
+    //   .end((err, res) => {
+    //     if (err) {
+    //       return err;
+    //     }
+    //
+    //     this.state.itemLists.splice(this.getIndexOfItem(id), 1, res.body);
+    //     this.setState({itemLists: this.state.itemLists});
+    //   })
+  }
+
+  deleteListItem(id) {
+    // request.delete(`http://192.168.30.11/todoitems/${id}`)
+    //   .end((err, res) => {
+    //     if (err) {
+    //       return err;
+    //     }
+    //
+    //     this.state.itemLists.splice(this.getIndexOfItem(id), 1);
+    //
+    //     this.setState({itemLists: this.state.itemLists});
+    //   })
+  }
+
+  render() {
+    return (
+      <div className="col-lg-offset-1">
+        <AddListItem addListItem={this.addListItem.bind(this)}/>
+        <ShowItemLists itemLists={this.state.itemLists} updateListItem={this.updateListItem.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('app')
+);
